608c559c42959932202d9a32f5ff36b14c29a3d3
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-08-30 13:27:25 -0600

    Add tables: treatments and invoices

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -12,3 +12,17 @@ CREATE TABLE patients (
     date_of_birth DATE
 );
 
+CREATE TABLE treatments (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    type VARCHAR(125),
+    name VARCHAR(125) 
+);
+
+CREATE TABLE invoices (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    total_amount DECIMAL(18,2),
+    generated_at TIMESTAMP,
+    payed_at TIMESTAMP,
+    medical_history_id INT,
+    CONSTRAINT FK_medical_histories FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id)
+);
