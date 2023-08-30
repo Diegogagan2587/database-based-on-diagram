c86ba144ff6111f806a28ab35412d09ba84740b9
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-08-30 13:17:24 -0600

    Add table: medical_histories

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,7 +1,14 @@
+CREATE TABLE medical_histories (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    admitted_at TIMESTAMP,
+    patient_id INT,
+    status VARCHAR(250),
+    CONSTRAINT FK_patients FOREIGN KEY (patient_id) REFERENCES patients(id)
+);
 
-
-CREATE TABLE PATIENTS (
+CREATE TABLE patients (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(125),
     date_of_birth DATE
-)
+);
+
